303bc7d8d782d332be84bc5ffa21bb05c3fe0561
michael guerg <michaelguerga@gmail.com>
2024-06-05 17:58:33 +0200

    php worksss

--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -1,7 +1,8 @@
-!# /bin/sh
 cd /etc/data && \
-wget https://wordpress.org/latest.tar.gz && \
-tar -xzvf latest.tar.gz && \
-rm latest.tar.gz && \
-chmod 777 * && \
-top
+wget https://wordpress.org/latest.tar.gz > /dev/null && \
+tar -xzvf latest.tar.gz > /dev/null && \
+rm latest.tar.gz > /dev/null && \
+chmod -R 777 * > /dev/null && \
+php-fpm83 -D && \
+top > /dev/null
+
